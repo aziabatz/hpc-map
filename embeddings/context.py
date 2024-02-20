a64683eab8e06dd5a0cc25c8fb5dd97f200cdc86
Ahmed Ziabat Ziabat <ahmed.ziabat@protonmail.com>
2024-02-20 18:24:03 +0100

    Context embeddings class

--- embeddings/context.py
+++ embeddings/context.py
@@ -8,5 +8,17 @@ from rl4co.models.nn.env_embeddings.context import EnvContext
 class MappingContextEmbedding(EnvContext):
 
     def __init__(self, embedding_dim, step_context_dim=None, linear_bias=False):
-        super(MappingContextEmbedding, self).__init__()
+        super(MappingContextEmbedding, self).__init__(
+            embedding_dim=embedding_dim,
+            step_context_dim=embedding_dim+1
+        )
+        
+    """
+        TODO: Here we can add from Node2Vec the embedded node vectors for the first and current node.
+        TODO: If dynamic embedding doesnt help, add node capacities as context for the problem partial solution
+        TODO: Also we can add the placement array as part of the context embedding
+    """
 
+    def _state_embedding(self, embeddings, td):
+        total_placed = td['total_placed']
+        return total_placed
