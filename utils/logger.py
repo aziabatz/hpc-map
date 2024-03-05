1aa880c992791c58150b2f63215861a82e514ca4
Ahmed Ziabat Ziabat <ahmed.ziabat@protonmail.com>
2024-03-05 14:24:22 +0100

    Add hydra and helper methods

---
+++ utils/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+from lightning.pytorch.utilities.rank_zero import rank_zero_only
+
+
+def get_pylogger(name=__name__) -> logging.Logger:
+
+    logger = logging.getLogger(name)
+
+    logging_levels = [
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "exception",
+        "fatal",
+        "critical",
+    ]
+
+    for level in logging_levels:
+        setattr(logger, level, rank_zero_only(getattr(logger, level)))
+
+    return logger
---
+++ utils/platform.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def get_device():
+    device = "cpu"
+    if torch.cuda.is_available():
+        if torch.backends.cuda.is_built():
+            device = "cuda"
+    elif torch.backends.mps.is_available():
+        if torch.backends.mps.is_built():
+            device = "mps"
+
+    return device
+
+
+def get_accelerator(device):
+
+    accelerator = None
+
+    if device == "cuda":
+        accelerator = "gpu"
+    elif device == "mps":
+        accelerator = device
+    else:
+        accelerator = "auto"
+
+    return accelerator
