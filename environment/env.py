31449b8e98798ea3aae8c0b499a9a5c12854654e
Ahmed Ziabat Ziabat <ahmed.ziabat@protonmail.com>
2024-03-14 17:23:01 +0100

    Add dynamic embedding

--- environment/env.py
+++ environment/env.py
@@ -262,7 +262,7 @@ class MappingEnv(RL4COEnvBase):
         # Generate distance matrices inspired by the reference MatNet (Kwon et al., 2021)
         # We satifsy the triangle inequality (TMAT class) in a batch
         batch_size = [batch_size] if isinstance(batch_size, int) else batch_size
-        dms = torch.randint(
+        dms: torch.Tensor= torch.randint(
             low=0,
             high=self.max_cost,
             size=(*batch_size, self.num_procs, self.num_procs),
@@ -279,7 +279,7 @@ class MappingEnv(RL4COEnvBase):
             < sparsity
         )
         # apply sparsity mask
-        dms.masked_fill(mask, 0)
+        dms.masked_fill_(mask, 0)
 
         return TensorDict(
             {
